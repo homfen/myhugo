83e3607aec4b70964d45b733e992ede992514334
hongfeng <hongfeng@baidu.com>
2017-11-02 16:10:30 +0800

    update

---
+++ tags/express/index.js
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Welcome to my blog.">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="generator" content="Hugo 0.19" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="/images/favicon.ico">
+<link rel="stylesheet" href="http://blog.homfen.me/css/sourcecodepro.css" type="text/css">
+<link rel="stylesheet" href="//static.homfen.me/plugin/highlight.js/dist/styles/solarized-dark.css">
+<link rel="stylesheet" href="http://blog.homfen.me/css/style.css" type="text/css">
+<link rel="alternate" href="http://blog.homfen.me/index.xml" type="application/rss+xml" title="Homfen&#39;s blog">
+<title>Express.Js - Homfen&#39;s blog</title>
+</head>
+<body>
+
+<header>
+  <div class="container clearfix">
+    <a class="path" href="http://blog.homfen.me/">[Homfen&#39;s blog]</a>
+    <span class="caret"># _</span>
+    <div class="right">
+      
+    </div>
+  </div>
+</header>
+
+<div class="container">
+
+
+
+<main role="main" class="homepage">
+
+    
+<h1 class="site-title">
+  <span class="base05">[</span><span class="base08">H</span><span class="base09">o</span><span class="base0a">m</span><span class="base0b">f</span><span class="base0c">e</span><span class="base0d">n</span><span class="base05">]</span>
+  <span class="base05"># _</span>
+</h1>
+
+<div class="hero-logo">
+  <img src="http://blog.homfen.me/img/base16-eighties.svg">
+</div>
+
+<div style="text-align: center;">
+  Stay hungry, stay foolish.
+</div>
+
+
+    <h1 class="list-title">Express.Js</h1>
+
+    <div class="article-list">
+        <article class="list-item" itemscope itemtype="http://schema.org/Blog">
+        
+            
+<h2 class="headline" itemprop="headline">
+    <a href="/2016/10/19/express.js-get-client-ip/">Express.js Get Client IP</a>
+    <div class="meta">
+    
+        <span class="key">published on</span>
+        <span class="val"><time itemprop="datePublished" datetime="2016-10-19">October 19, 2016</time></span>
+    
+    </div>
+</h2>
+
+        
+        </article>
+    </div>
+
+</main>
+
+
+</div>
+
+<footer>
+  <div class="container">
+  <span class="copyright">&copy; <script>document.write((new Date()).getFullYear())</script> Homfen&#39;s blog - <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a></span>
+  </div>
+</footer>
+
+<script src="//static.homfen.me/plugin/highlight.js/dist/highlight.pack.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?ea23518b0514a7b24922188359137040";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+</body>
+</html>
+
